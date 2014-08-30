fc228e59be56b3cc8310cb58d3093e71e68f17fc
Yi <yil712@lehigh.edu>
2014-08-30 01:11:38 -0400

    Added the simple solution to the kaggle bike sharing problem.

---
+++ src/kaggle-bike-sharing/gbrKaggle.py
@@ -0,0 +1,24 @@
+# by Yi Luo
+
+import numpy as np
+from sklearn import ensemble
+
+###############################################################################
+# Load data
+trainset = np.loadtxt("kaggleTrain.csv", delimiter=',')
+sh = trainset.shape
+m = sh[0]
+X_train, y_train = trainset[:,0:8], trainset[:,9]
+
+testset = np.loadtxt("kaggleTest.csv", delimiter=',')
+X_test = testset[:, 0:8]
+###############################################################################
+# Fit regression model
+params = {'n_estimators': 500, 'max_depth': 10, 'min_samples_split': 1,
+          'learning_rate': 0.01, 'loss': 'ls'}
+clf = ensemble.GradientBoostingRegressor(**params)
+clf.fit(X_train, y_train)
+
+y_predict = clf.predict(X_test)
+
+np.savetxt("test_output.csv", y_predict, delimiter=",")
